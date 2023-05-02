4f69f1befb4e66086846ecae75421a409e9f4be4
Adrian <blancomartinadrian@gmail.com>
2023-05-02 11:35:35 +0200

    commit 3 bbdd

--- abrirConexion.php
+++ abrirConexion.php
@@ -5,7 +5,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'madridskills');
+define('DB_NAME', 'akari_desing');
  
 /* Attempt to connect to MySQL database */
 $conexion = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
